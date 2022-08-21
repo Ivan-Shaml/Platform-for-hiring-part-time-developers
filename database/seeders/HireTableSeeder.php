89f3f71cd18ffe09ead144029c52f508ecd161ce
Evgeni-Georgiev <evgenigg2000@gmal.com>
2022-08-21 21:05:08 +0300

    Hired developer factory for generating fake data and seeder.

---
+++ database/seeders/HireTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Developer;
+use App\Models\Hire;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class HireTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Hire::factory()->count(5)->create();
+    }
+}
